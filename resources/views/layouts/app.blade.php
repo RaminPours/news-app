fac4da85e16e002fefc7f4f1020392a318042394
RaminPours <r.poursakha@gmail.com>
2026-08-24 15:12:31 +0200

    news-app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,21 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>layout app</title>
 </head>
 <body>
     <header>
         <x-navbar/>
     </header>
-    
 
     <main>
           @yield('content')
     </main>
-    
+
     <footer>
         @yield('foot')
     </footer>
+    
     
 </body>
 </html>
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -14,36 +14,37 @@
 </head>
 <body class="bg-light text-2xl">
 
-    @extends('layouts.app')
+@extends('layouts.app')
 
-    @section('content')
-    <div class="container mt-5">
-        <h1 class="text-center mb-4">Tech Nieuws</h1>
-        <div class="row">
-            @foreach ($techArticles['articles'] as $article)
-                <div class="col-md-4 mb-4">
-                    <div class="card h-100">
-                        @if ($article['urlToImage'])
-                            <img src="{{ $article['urlToImage'] }}" class="card-img-top" alt="...">
-                        @endif
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $article['title'] }}</h5>
-                            <p class="card-text">{{ $article['description'] }}</p>
-                            <a href="{{ $article['url'] }}" target="_blank" class="btn btn-primary">Read More</a>
-                        </div>
+@section('content')
+<div class="container mt-5">
+    <h1 class="text-center mb-4">Tech Nieuws</h1>
+    <div class="row">
+        @foreach ($techArticles['articles'] as $article)
+            <div class="col-md-4 mb-4">
+                <div class="card h-100">
+                    @if ($article['urlToImage'])
+                        <img src="{{ $article['urlToImage'] }}" class="card-img-top">
+                    @endif
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $article['title'] }}</h5>
+                        <p class="card-text">{{ $article['description'] }}</p>
+                        <a href="{{ $article['url'] }}" target="_blank" class="btn btn-primary">Read More</a>
                     </div>
                 </div>
-            @endforeach
-        </div>
-        @endsection
-        
-
-       
-    @section('foot')
-    <footer class="bg-dark text-white text-center py-3 mt-6">
-        <p>&copy; 2024 News App. All rights reserved.</p>
-    </footer>
+            </div>
+        @endforeach
+    </div>
     @endsection
+    
+@section('foot')
+<footer class="bg-dark text-white text-center py-3 mt-6">
+    <p>&copy; 2024 News App. All rights reserved.</p>
+</footer>
+@endsection
+    
+
+    
 </body>
 
 </html>
